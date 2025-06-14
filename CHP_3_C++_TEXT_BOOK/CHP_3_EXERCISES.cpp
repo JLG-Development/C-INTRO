c137f543e46ae2cd834cab36a3c4da7ee5dfae41
Jeremiah G. <JeremiahGio@outlook.com>
2025-06-14 16:41:56 -0500

    exercises

--- CHP_3_C++_TEXT_BOOK/CHP_3_EXERCISES.cpp
+++ CHP_3_C++_TEXT_BOOK/CHP_3_EXERCISES.cpp
@@ -35,18 +35,24 @@ void exercise3(){
     double median = 0.0;
     double temp = 0.0;
 
-    std::vector<double> v = {2, 3, 4, 5, 6};
+    std::vector<double> v = {1, 2, 3, 4};
+    /*14, 83, 38, 5, 65, 9, 62, 50, 1, 12, 31, 93, 80, 20, 95, 72, 22, 55, 43, 64,
+    63, 16, 59, 7, 41, 49, 78, 71, 81, 73, 75, 46, 92, 26, 34, 24, 32, 23, 3, 60,
+    33, 42, 11, 79, 10, 35, 70, 76, 45, 21, 84, 86, 18, 36, 66, 27, 90, 29, 19, 39,
+    96, 98, 8, 17, 74, 68, 44, 91, 85, 13, 6, 100, 28, 99, 25, 15, 77, 48, 4, 97,
+    87, 40, 30, 58, 94, 47, 2, 37, 69, 67, 61, 88, 53, 52, 51, 57, 54, 56, 82, 89
+    };*/
     sortVector(v);
 
-    std::cout << v.size() << "\n";
+    //std::cout << v.size() << "\n";
 
-    std::cout << "The vector: \n";
+    std::cout << "The Final vector: \n";
     for(double num : v){
         std::cout << num << ", ";
     }
     std::cout << "\n";
 
-   /* if(v.size() % 2 == 0){
+   if(v.size() % 2 == 0){
         std::cout << "IM WORKING\n";
         //take the two middle elements, add them together and dvide by 2
         //pull the middle element by taking size dividing by two and pull one to the right as well
@@ -56,7 +62,7 @@ void exercise3(){
         //just pull middle element
         median = v[(v.size() / 2) + 1];
     }
-    std::cout << "Median is " << median << ".\n"; */
+    std::cout << "Median is " << median << ".\n"; 
 }
 
 //change to just set the values not insert and erase. This will eliminate the off by one error that 
@@ -70,34 +76,30 @@ void sortVector(std::vector<double>& v){
         num1 = v[0];
         v.erase(v.begin() + 0);
 
-        for(int j = 0; j < v.size() - i; j++){
-            std::cout << "index: " << j << "(" << num1 << ")" << " -----> ";
-            if(num1 < v[j] && j != v.size() - i - 1){
+        for(int j = 0; j <= v.size() - i; j++){//v <= so it actually compares to six on first iteration
+            //std::cout << "index: " << j << "(" << num1 << ")" << " -----> ";
+            if(num1 < v[j]){
                 std::cout << j << " swap\n";
                 displaced = v[j];
                 v[j] = num1;
                 num1 = displaced;
-            }
-            if(j == v.size() - i - 1  && i != 0){
-                std::cout << j << " inserted\n";
+            }else if(j == v.size() - i  && i != 0){
+                //std::cout << j << " inserted\n";
                 v.insert(v.begin() + j + 1, num1);
                 break;
-            }
+            }else if(i == 0){
+                //std::cout << "index: " << v.size()-1 << "(" << num1 << ")" << " -----> inserted on end\n";
+                v.push_back(num1);
+                break;
+            }    
         }
-        if(i == 0){
-            std::cout << "index: " << v.size()-1 << "(" << num1 << ")" << " -----> inserted on end\n";
-            v.push_back(num1);
-        }
+       /*
         std::cout << "The vector: \n";
         for(double num : v){
             std::cout << num << ", ";
         }
-        std::cout << "\n";
-
-        std::cout << "\n\n+---------------------------+\ninner loop fully iterated + " << i << "\n" ;
-        
-       
+        std::cout << "\n\n+---------------------------+\ninner loop fully iterated + " << i << "\n" ;     
+        */
     }
-
     v.insert(v.begin() + 0, num1);
 }
